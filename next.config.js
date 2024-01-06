28890c37a28768a13f92155b5154e4a8a07347b2
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2024-01-06 18:44:55 -0300

    feat: Creating complex form and markdown

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "hpm58d6xookcs2il.public.blob.vercel-storage.com" },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/app/job-submitted/page.tsx
@@ -0,0 +1,10 @@
+import H1 from "@/components/ui/h1";
+
+export default function JobSubmitPage() {
+  return (
+    <main className="sapce-y-5 m-auto my-10 max-w-5xl px-3 text-center">
+      <H1>Job submitted</H1>
+      <p>Your job posting has been submitted and is pending approval</p>
+    </main>
+  );
+}
