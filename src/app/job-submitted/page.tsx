86d2f977e54e7c63e2141fe4c06912561bd0316d
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2024-01-07 12:08:00 -0300

    fix: typo

--- src/app/job-submitted/page.tsx
+++ src/app/job-submitted/page.tsx
@@ -2,7 +2,7 @@ import H1 from "@/components/ui/h1";
 
 export default function JobSubmitPage() {
   return (
-    <main className="sapce-y-5 m-auto my-10 max-w-5xl px-3 text-center">
+    <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center">
       <H1>Job submitted</H1>
       <p>Your job posting has been submitted and is pending approval</p>
     </main>
